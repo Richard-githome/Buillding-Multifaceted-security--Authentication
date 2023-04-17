4c8bff8f2db51caa86aebf859b50a5ba3efedec1
Emeka <macbook@MACBOOKs-MacBook-Air.local>
2023-04-17 16:23:07 +0100

    moved session_secret key to environment

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(
   session({
-    secret: "This is the secret key",
+    secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
   })
